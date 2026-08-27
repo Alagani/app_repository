b9a3fc90bd6e166d87e58f99c6349188d7a3a91f
Alagani Jagadeesh <alaganijagadeesh1@gmail.com>
2026-08-27 17:51:04 +0530

    remove test

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,9 +9,3 @@ def test_root() -> None:
     resp = client.get("/")
     assert resp.status_code == 200
     assert resp.json() == {"status": "ok"}
-
-
-def test_healthz() -> None:
-    resp = client.get("/healthz")
-    assert resp.status_code == 200
-    assert resp.json() == {"status": "ok jagadeesh"}
